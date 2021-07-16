102da0b3877d8f3f262bc0a984f3f6215d8154d1
abhishekc <abhishekc@qburst.com>
2021-07-16 10:36:28 +0530

    Changed Local PORT from 5000 to 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
