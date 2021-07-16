f39b41ac81cef9608417f2ba6f17c3ed55b96e39
abhishekc <abhishekc@qburst.com>
2021-07-16 09:51:43 +0530

    Created the base file for connecting with database and other routes

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database connected'));
+
+app.use(cors({ origin: '*' }));
+
+const userIpRouter = require('./routes/userIp');
+app.use('/userip', userIpRouter);
+
+app.listen(3000, () => console.log('Server Started'));
