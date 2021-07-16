f7b44a55cc35ea01653daf48a9a24411305b82ae
abhishekc <abhishekc@qburst.com>
2021-07-16 12:05:03 +0530

    Added routes to get the user and post userid and current userip.

--- routes/userIp.js
+++ routes/userIp.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const UserIp = require('../models/userIp');
 
-router.get('/getuser', async (req, res) => {
+router.get('/', async (req, res) => {
 	try {
 		const userIps = await UserIp.find();
 		res.status(200).json(userIps);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+router.post('/userip', async (req, res) => {
+	const userIp = new UserIp({ userId: req.body.userId, ipAddress: req.body.ip });
+	try {
+		const newUserIp = await userIp.save();
+		res.status(200).json(newUserIp);
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
